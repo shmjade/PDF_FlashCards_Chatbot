afa1a7553697168bb813534042cfe205cf79e0c1
Jade Satyko Hatanaka Marques <jade-satyko.hatanaka-marques@grenoble-inp.org>
2024-04-05 16:39:27 +0200

    use OPENAI as llm

--- multiple_pdfs.py
+++ multiple_pdfs.py
@@ -9,7 +9,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
-
+import chromadb
 
 def get_pdf_text(pdf_docs):
     text = ""
